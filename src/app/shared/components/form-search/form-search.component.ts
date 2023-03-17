be4cf3d61fed556e3a13fa03e9116328c655f5b8
walisson-gonz <walisson.gonzales@gmail.com>
2023-03-17 03:03:36 -0300

    Formando Header e Input

--- src/app/shared/components/form-search/form-search.component.ts
+++ src/app/shared/components/form-search/form-search.component.ts
@@ -1,10 +1,28 @@
+import { Router} from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-form-search',
-  templateUrl: './form-search.component.html',
+  template: `
+  <input
+  #inputSearch 
+  autofocustype="text"
+  class="form-control-lg w-100"
+  placeholder="Search........"
+  (keyup)="onSearch(inputSearch.value)"
+  />
+  `,
   styleUrls: ['./form-search.component.css']
 })
 export class FormSearchComponent {
+constructor(private router:Router) {}
 
+  onSearch(value: string){
+    console.log('Buscar-->',value)
+    if(value && value.length > 3){
+      this.router.navigate(['/character-list'], {
+        queryParams: {q: value},
+      })
+    }
+  }
 }
